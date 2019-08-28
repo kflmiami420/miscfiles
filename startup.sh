37b3208e06e8a8ca6ba08c54715f914245879cc5
KFLMIAMI420 <42599019+kflmiami420@users.noreply.github.com>
2019-08-28 00:14:29 -0400

    Create startup.sh
    stats.py   autostart.sh

---
+++ startup.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# How To Autostart Apps In Rasbian LXDE Desktop
+# http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxd$
+
+echo "Starting "
+/home/pi/stats.py
